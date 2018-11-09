20649f92534aa3f586227c8c1c59b13eeff3ffcb
Jackson L. Cole <jackson.lane.cole@gmail.com>
2018-11-09 16:34:11 -0600

    Updated run_cmake.sh to accept an argument and either run cmake or ONLY make depending

--- scripts/run_cmake.sh
+++ scripts/run_cmake.sh
@@ -4,14 +4,21 @@ PWD=$(PWD)
 SCRIPTSDIR=$PWD/scripts
 mkdir -p build
 cd build
-sh $SCRIPTSDIR/clean.sh
+if [ $1 -eq 1 ]
+then
+	echo "Running CMake in build directory..."
+	sh $SCRIPTSDIR/clean.sh
 
-cmake -DPYTHON_LIBRARY=/usr/local/Cellar/python3/3.7.0/Frameworks/Python.framework/Versions/3.7/lib/libpython3.7m.dylib \
-	  -DPYTHON_INCLUDE_DIR=/usr/local/Cellar/python/3.7.0/Frameworks/Python.framework/Versions/3.7/include/python3.7m \
-	  -DBOOST_ROOT=/usr/local/Cellar/boost/1.67.0_1 \
-      -DBOOST_INCLUDEDIR=/usr/local/Cellar/boost/1.67.0_1/include \
-	  -DBOOST_LIBRARYDIR=/usr/local/Cellar/boost-python3/1.67.0_1/lib \
-	  ..
+	cmake -DPYTHON_LIBRARY=/usr/local/Cellar/python3/3.7.0/Frameworks/Python.framework/Versions/3.7/lib/libpython3.7m.dylib \
+		  -DPYTHON_INCLUDE_DIR=/usr/local/Cellar/python/3.7.0/Frameworks/Python.framework/Versions/3.7/include/python3.7m \
+		  -DBOOST_ROOT=/usr/local/Cellar/boost/1.67.0_1 \
+		  -DBOOST_INCLUDEDIR=/usr/local/Cellar/boost/1.67.0_1/include \
+		  -DBOOST_LIBRARYDIR=/usr/local/Cellar/boost-python3/1.67.0_1/lib \
+		  ..
+	echo "Running make in build directory..."
+else
+	echo "Running make in build directory..."
+fi
 
 make
 
